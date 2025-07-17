741dff1e99c56dbd3dc61b58363bcd700f23b818
EAAR-300519 <aspire5@outlook.es>
2025-07-16 22:54:05 -0600

    Hicimos que el endpoint registre usuario en la base de datos con validaciones basicas

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,7 +1,35 @@
-function register(req, res) {
-  console.log("Se ha ejecutado correctamente");
+const bcrypt = require("bcryptjs");
+const User = require("../models/UserModel");
 
-  res.status(200).json({ msg: "Todo Ok!" });
+async function register(req, res) {
+  try {
+    const { firstname, lastname, email, password } = req.body;
+
+    if (!email) {
+      res.status(404).json({ msg: "El email es obligarotio." });
+    }
+    if (!password) {
+      res.status(404).json({ msg: "El password es obligarotio." });
+    }
+
+    const user = new User({
+      firstname,
+      lastname,
+      email: email.toLowerCase(),
+      role: "user",
+      active: true,
+    });
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(password, salt);
+
+    user.password = hashPassword;
+
+    const userStorage = await user.save();
+    res.status(200).json(userStorage);
+  } catch (error) {
+    res.status(400).json({ msg: "El usuario ya existe!" });
+  }
 }
 
 module.exports = {
