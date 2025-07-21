ab7b97631d853fcf8fc4bc1714eedd9fac4f2e67
Alejandro Alcocer <ealcocer@kapital.com>
2025-07-21 16:26:41 -0600

    Obtenemos una lista con todos los usuarios dependiendo si estan activos o no

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,16 +1,36 @@
 const UserModel = require("../models/UserModel");
 
 async function getMe(req, res) {
-  const { user_id } = req.user;
-  const response = await UserModel.findById(user_id);
+  try {
+    const { user_id } = req.user;
+    const response = await UserModel.findById(user_id);
+    if (!response) {
+      res.status(400).json({ msg: "No se ha encontrado el usuario!" });
+    } else {
+      res.status(200).json(response);
+    }
+  } catch (error) {
+    res.status(500).json({ msg: "Hubo un problema en el servidor..." });
+  }
+}
 
-  if (!response) {
-    res.status(400).json({ msg: "No se ha encontrado el usuario!" });
-  } else {
+async function getUsers(req, res) {
+  try {
+    const { active } = req.query;
+    let response = null;
+
+    if (active === undefined) {
+      response = await UserModel.find();
+    } else {
+      response = await UserModel.find({ active });
+    }
     res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: "Hubo un error en el servicio..." });
   }
 }
 
 module.exports = {
   getMe,
+  getUsers,
 };
